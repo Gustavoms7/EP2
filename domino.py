d968dda6dddc429a089918d1733b7d9675655706
Gustavo <gustavoms7@al.insper.edu.br>
2021-11-07 19:23:59 -0300

    arrumando os prints

--- domino.py
+++ domino.py
@@ -8,7 +8,6 @@ print('Vamos começar!!!')
  
 
 #  perguntar quantos jogadores 
-
 numero_jogadores = int(input('Quantas jogadores ? (2-4)'))
 
 while numero_jogadores <= 1 or numero_jogadores >= 5 :
@@ -22,13 +21,14 @@ pecas=Funcoes.cria_pecas()
 domino = Funcoes.inicia_jogo(numero_jogadores,pecas)
 adiciona = domino['mesa']
 monte = domino['monte']
+
 i= random.randint(0,numero_jogadores)
 while i<=numero_jogadores:
-    print(f'Mesa: {adiciona}')
     if i==numero_jogadores:
         i=0
     elif i==0:
         pecas = domino['jogadores'][i]
+        print(f'Mesa: {adiciona}')
         print(f'Minhas peças: {pecas}')
         posicoes_possiveis = Funcoes.posicoes_possiveis(adiciona,pecas)
         while True:    
@@ -45,13 +45,16 @@ while i<=numero_jogadores:
         peca_escolhida = pecas[peca]
         print(f'Peça Escolhida: {peca_escolhida}')
         adiciona = Funcoes.adiciona_na_mesa(peca_escolhida,adiciona)
-        print(f'Mesa: {adiciona}')
         del pecas[peca]
-        print(f'Minhas peças: {pecas}')
-        i+=1
+        ganhador=Funcoes.verifica_ganhador(domino)
+        if ganhador==-1:
+            i+=1
+        else:
+            print(f'Mesa: {adiciona}')
+            break
     elif i !=0 :
         pecas = domino['jogadores'][i]
-        print(f'Peças do jogador {i} : {pecas}')
+        print(f'Mesa: {adiciona}')
         posicoes_possiveis = Funcoes.posicoes_possiveis(adiciona,pecas)
         while True:    
             if posicoes_possiveis==[]:
@@ -60,18 +63,19 @@ while i<=numero_jogadores:
                 del monte[0]
                 print(pecas)
             else:
-                print(f'Posições Possíveis: {posicoes_possiveis}')
                 break
-        
         peca = random.randint(0,len(posicoes_possiveis)-1)
         peca = posicoes_possiveis[peca]
         peca_escolhida = pecas[peca]
         print(f'Peça Escolhida: {peca_escolhida}')
         adiciona = Funcoes.adiciona_na_mesa(peca_escolhida,adiciona)
-        print(f'Mesa: {adiciona}')
         del pecas[peca]
-        print(f'Peças do jogador {i}: {pecas}')
-        i+=1
+        ganhador=Funcoes.verifica_ganhador(domino)
+        if ganhador==-1:
+            i+=1
+        else:
+            print(f'Mesa: {adiciona}')
+            break
     
 
 
@@ -84,11 +88,11 @@ lista_soma = []
 for pecas_jogador in domino['jogadores'].values():
     soma = Funcoes.soma_pecas(pecas_jogador)
     lista_soma.append(soma)
-posicoes_possiveis = Funcoes.posicoes_possiveis(mesa,pecas)
+posicoes_possiveis = Funcoes.posicoes_possiveis(adiciona,pecas)
 
 
 #Posições possíveis da mão
-posicoes_possiveis = Funcoes.posicoes_possiveis(mesa,pecas)
+posicoes_possiveis = Funcoes.posicoes_possiveis(adiciona,pecas)
 #Adicionando peças a mesa num jogo de dominó
 for pecas_jogador in domino['jogadores'].values():
-    adiciona = Funcoes.adiciona_na_mesa(pecas_jogador,mesa)
+    adiciona = Funcoes.adiciona_na_mesa(pecas_jogador,adiciona)
