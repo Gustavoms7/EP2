054bc99f32ae77405bdbafb34fd18fd6524830d3
Kevin Shinohara <kevinns@al.insper.edu.br>
2021-11-07 03:21:19 -0300

    Introdução

---
+++ introdução.py
@@ -0,0 +1,5 @@
+print("Insper Dominó")
+print('=-'*70)
+print('Bem-vindo(a) ao jogo de Dominó! O objetivo desse jogo é ficar sem peças na sua mão antes dos outros jogadores.')
+print('Vamos começar!!!')
+ 
