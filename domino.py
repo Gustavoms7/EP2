b931a566751e7b95d6db48bf34d9131ce16dcc18
Gustavo <gustavoms7@al.insper.edu.br>
2021-11-07 10:12:32 -0300

    quantas pessoas vão jogar

--- domino.py
+++ domino.py
@@ -5,3 +5,14 @@ print('=-'*70)
 print('Bem-vindo(a) ao jogo de Dominó! O objetivo desse jogo é ficar sem peças na sua mão antes dos outros jogadores.')
 print('Vamos começar!!!')
  
+
+#  perguntar quantos jogadores 
+
+numero_jogadores = int(input('Quantas pessoas vão jogar ?'))
+
+while numero_jogadores <= 1 and numero_jogadores >= 5 :
+    print('Número inválido!')
+
+    numero_jogadores = int(input('Quantas pessoas vão jogar ?'))
+
+
