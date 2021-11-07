ae72da32d328788219476ebd523a6a2913cbfd50
Gustavo <gustavoms7@al.insper.edu.br>
2021-11-07 09:56:44 -0300

    jogo final

---
+++ domino.py
@@ -0,0 +1,7 @@
+
+
+print("Insper Dominó")
+print('=-'*70)
+print('Bem-vindo(a) ao jogo de Dominó! O objetivo desse jogo é ficar sem peças na sua mão antes dos outros jogadores.')
+print('Vamos começar!!!')
+ 
